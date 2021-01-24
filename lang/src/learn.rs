517d2eacb1c88f9b8fcb1a2a1462cd0f5527907b
y² <yy@yuyuan.org>
2021-01-23 19:29:16 -0800

    Move learning code to unit tests.

--- lang/src/learn.rs
+++ lang/src/learn.rs
@@ -1,40 +1,77 @@
-pub fn string_literal_type() {
-    let s = "Hello";  // s type is &str
+pub fn __________() {
+    eprintln!("{}", ["-"; 80].concat());
 
-    // |     let i: i32 = s
-    // |            ---   ^ expected `i32`, found `&str`
-    // |            |
-    // |            expected due to this
-
-    println!("{}", s);
-    println!("{}", s);
 }
 
-pub fn string_from() {
-    let s: String = String::from("hello");
-
-    // |     let i: i32 = s
-    // |            ---   ^ expected `i32`, found struct `String`
-    // |            |
-    // |            expected due to this
-
-    println!("{}", s);
-    println!("{}", s);
+pub fn __________2() {
+    eprintln!("{}", ["="; 80].concat());
 }
 
-pub fn string_slice() {
-    let s = String::from("hello");
 
-    // |     let i: i32 = &s[0..2];
-    // |            ---   ^^^^^^^^ expected `i32`, found `&str`
-    // |            |
-    // |            expected due to this
+#[cfg(test)]
+mod tests {
+    use super::__________;
+    use super::__________2;
 
-    let ss = &s[0..2];
+    #[test]
+    fn string_literal_type() {
+        __________();
+        let s = "Hello";  // s type is &str
 
-    println!("{}", s);
-    println!("{}", ss);
-    println!("{}", s);
-    println!("{}", ss);
+        // |     let i: i32 = s
+        // |            ---   ^ expected `i32`, found `&str`
+        // |            |
+        // |            expected due to this
+
+        println!("{}", s);
+        println!("{}", s);
+        __________2();
+    }
+
+    #[test]
+    fn string_from() {
+        __________();
+        let s: String = String::from("hello");  // s type is String
+
+        // |     let i: i32 = s
+        // |            ---   ^ expected `i32`, found struct `String`
+        // |            |
+        // |            expected due to this
+
+        println!("{}", s);
+        println!("{}", s);
+        __________2();
+    }
+
+    #[test]
+    fn string_slice() {
+        __________();
+        let s = String::from("hello");  // s type is String
+        let ss = &s[0..2];  // ss type is &str
+
+        // |     let i: i32 = &s[0..2];
+        // |            ---   ^^^^^^^^ expected `i32`, found `&str`
+        // |            |
+        // |            expected due to this
+
+        println!("{}", s);
+        println!("{}", ss);
+        println!("{}", s);
+        println!("{}", ss);
+        __________2();
+    }
+
+    #[test]
+    fn use_crate_learn() {
+        use crate::learn;
+        learn::__________();
+        super::__________2();
+    }
+
+    #[test]
+    fn full_path() {
+        crate::learn::__________();
+        super::__________2();
+    }
 }
 
--- lang/src/main.rs
+++ lang/src/main.rs
@@ -3,9 +3,5 @@ mod learn;
 fn main() {
     println!("Hello, world!");
 
-    learn::string_literal_type();
-
-    learn::string_from();
-
-    learn::string_slice();
+    learn::__________();
 }
