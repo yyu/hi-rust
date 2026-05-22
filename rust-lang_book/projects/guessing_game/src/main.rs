542612f1c0ffc004ec338224d4b1dec90d093830
y² <yy@yuyuan.org>
2026-05-22 07:50:59 +0000

    Continue with guessing game

--- rust-lang_book/projects/guessing_game/src/main.rs
+++ rust-lang_book/projects/guessing_game/src/main.rs
@@ -12,29 +12,34 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    // If we hadn’t imported the `io` module with use `std::io;` at the beginning of the program,
-    // we could still use the function by writing this function call as `std::io::stdin`.
-    // The stdin function returns an instance of `std::io::Stdin`, which is a type that
-    // represents a handle to the standard input for your terminal.
-    io::stdin()
-        // The `&` indicates that this argument is a *reference*.
-        // References are immutable by default.
-        // Write `&mut guess` rather than `&guess` to make it mutable.
-        // `read_line()` returns an *enumeration*, aka *enum*.
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        // If we hadn’t imported the `io` module with use `std::io;` at the beginning of the program,
+        // we could still use the function by writing this function call as `std::io::stdin`.
+        // The stdin function returns an instance of `std::io::Stdin`, which is a type that
+        // represents a handle to the standard input for your terminal.
+        io::stdin()
+            // The `&` indicates that this argument is a *reference*.
+            // References are immutable by default.
+            // Write `&mut guess` rather than `&guess` to make it mutable.
+            // `read_line()` returns an *enumeration*, aka *enum*.
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
