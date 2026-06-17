ded8be3317d612c9f4436705dd4a330c359bb927
y² <yy@yuyuan.org>
2026-06-17 23:51:35 +0000

    elevator

--- comprehensive_rust/elevator/src/main.rs
+++ comprehensive_rust/elevator/src/main.rs
@@ -1,3 +1,57 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
+/// An event in the elevator system that the controller must react to.
+enum Event {
+    CarArrived { floor: i32},
+    CarDoorEvent { is_open: bool },
+    LobbyCallButtonPressed { floor: i32, dir: Direction },
+    CarFloorButtonPressed { floor: i32 },
+}
+
+/// A direction of travel.
+#[derive(Debug)]
+enum Direction {
+    Up,
+    Down,
+}
+
+/// The car has arrived on the given floor.
+fn car_arrived(floor: i32) -> Event {
+    todo!()
+}
+
+/// The car doors have opened.
+fn car_door_opened() -> Event {
+    todo!()
+}
+
+/// The car doors have closed.
+fn car_door_closed() -> Event {
+    todo!()
+}
+
+/// A directional button was pressed in an elevator lobby on the given floor.
+fn lobby_call_button_pressed(floor: i32, dir: Direction) -> Event {
+    todo!()
+}
+
+/// A floor button was pressed in the elevator car.
+fn car_floor_button_pressed(floor: i32) -> Event {
+    todo!()
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "A ground floor passenger has pressed the up button: {:?}",
+        lobby_call_button_pressed(0, Direction::Up)
+    );
+    println!("The car has arrived on the ground floor: {:?}", car_arrived(0));
+    println!("The car door opened: {:?}", car_door_opened());
+    println!(
+        "A passenger has pressed the 3rd floor button: {:?}",
+        car_floor_button_pressed(3)
+    );
+    println!("The car door closed: {:?}", car_door_closed());
+    println!("The car has arrived on the 3rd floor: {:?}", car_arrived(3));
 }
