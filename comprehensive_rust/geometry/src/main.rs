90e3d42bdb20f56366c3d1354a70b41857212a76
y² <yy@yuyuan.org>
2026-06-17 22:01:03 +0000

    geometry

--- comprehensive_rust/geometry/src/main.rs
+++ comprehensive_rust/geometry/src/main.rs
@@ -1,3 +1,23 @@
+// Calculate the magnitude of a vector by summing the squares of its coordinates
+// and taking the square root. Use the `sqrt()` method to calculate the square
+// root, like `v.sqrt()`.
+fn magnitude(v: &[f64; 3]) -> f64 {
+    v.iter().map(|x| x * x).sum::<f64>().sqrt()
+}
+
+// Normalize a vector by calculating its magnitude and dividing all of its
+// coordinates by that magnitude.
+fn normalize(v: &mut [f64; 3]) {
+    let mag = magnitude(v);
+    v.iter_mut().for_each(|x| *x /= mag);
+}
+
+// Use the following `main` to test your work.
 fn main() {
-    println!("Hello, world!");
+    println!("Magnitude of a unit vector: {}", magnitude(&[0.0, 1.0, 0.0]));
+
+    let mut v = [1.0, 2.0, 9.0];
+    println!("Magnitude of {v:?}: {}", magnitude(&v));
+    normalize(&mut v);
+    println!("Magnitude of {v:?} after normalization: {}", magnitude(&v));
 }
