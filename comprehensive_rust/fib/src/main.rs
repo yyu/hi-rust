f5b6c9389d77aff1d65bd66210b01aa6dbeb57a0
y² <yy@yuyuan.org>
2026-06-17 18:11:25 +0000

    fib

--- comprehensive_rust/fib/src/main.rs
+++ comprehensive_rust/fib/src/main.rs
@@ -2,7 +2,6 @@ fn fib(n: u32) -> u32 {
     if n < 2 {
         return n;
     } else {
-        // The recursive case.
         return fib(n - 1) + fib(n - 2);
     }
 }
@@ -11,7 +10,7 @@ fn main() {
     let n = 20;
     println!("fib({n}) = {}", fib(n));
 
-    for i in 0..10 {
+    for i in 0..20 {
         println!("fib({i}): {}", fib(i));
     }
 }
