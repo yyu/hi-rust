326c4abd19983cb730574b900cdf7f09cecd3fbf
y² <yy@yuyuan.org>
2026-06-18 21:04:07 +0000

    logger

--- comprehensive_rust/logger/src/main.rs
+++ comprehensive_rust/logger/src/main.rs
@@ -1,3 +1,32 @@
+trait Logger {
+    /// Log a message at the given verbosity level.
+    fn log(&self, verbosity: u8, message: &str);
+}
+
+struct StderrLogger;
+
+impl Logger for StderrLogger {
+    fn log(&self, verbosity: u8, message: &str) {
+        eprintln!("verbosity={verbosity}: {message}");
+    }
+}
+
+/// Only log messages up to the given verbosity level.
+struct VerbosityFilter {
+    max_verbosity: u8,
+    inner: StderrLogger,
+}
+
+impl Logger for VerbosityFilter {
+    fn log(&self, verbosity: u8, message: &str) {
+        if verbosity <= self.max_verbosity {
+            self.inner.log(verbosity, message);
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let logger = VerbosityFilter { max_verbosity: 3, inner: StderrLogger };
+    logger.log(5, "FYI");
+    logger.log(2, "Uhoh");
 }
