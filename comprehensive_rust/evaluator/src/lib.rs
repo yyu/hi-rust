a1ad8f7b5c3e0ab96961ad9ec8a41c718b020cd7
y² <yy@yuyuan.org>
2026-06-18 18:47:18 +0000

    evaluator

--- comprehensive_rust/evaluator/src/lib.rs
+++ comprehensive_rust/evaluator/src/lib.rs
@@ -1,14 +1,115 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+/// An operation to perform on two subexpressions.
+#[derive(Debug)]
+enum Operation {
+    Add,
+    Sub,
+    Mul,
+    Div,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// An expression, in tree form.
+#[derive(Debug)]
+enum Expression {
+    /// An operation on two subexpressions.
+    Op { op: Operation, left: Box<Expression>, right: Box<Expression> },
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// A literal value
+    Value(i64),
+}
+
+fn eval(e: Expression) -> i64 {
+    match e {
+        Expression::Value(val) => return val,
+        Expression::Op { op, left, right } => {
+            match op {
+                Operation::Add => return eval(*left) + eval(*right),
+                Operation::Sub => return eval(*left) - eval(*right),
+                Operation::Mul => return eval(*left) * eval(*right),
+                Operation::Div => return eval(*left) / eval(*right),
+            }
+        }
     }
+}
+
+#[test]
+fn test_value() {
+    assert_eq!(eval(Expression::Value(19)), 19);
+}
+
+#[test]
+fn test_sum() {
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Add,
+            left: Box::new(Expression::Value(10)),
+            right: Box::new(Expression::Value(20)),
+        }),
+        30
+    );
+}
+
+#[test]
+fn test_recursion() {
+    let term1 = Expression::Op {
+        op: Operation::Mul,
+        left: Box::new(Expression::Value(10)),
+        right: Box::new(Expression::Value(9)),
+    };
+    let term2 = Expression::Op {
+        op: Operation::Mul,
+        left: Box::new(Expression::Op {
+            op: Operation::Sub,
+            left: Box::new(Expression::Value(3)),
+            right: Box::new(Expression::Value(4)),
+        }),
+        right: Box::new(Expression::Value(5)),
+    };
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Add,
+            left: Box::new(term1),
+            right: Box::new(term2),
+        }),
+        85
+    );
+}
+
+#[test]
+fn test_zeros() {
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Add,
+            left: Box::new(Expression::Value(0)),
+            right: Box::new(Expression::Value(0))
+        }),
+        0
+    );
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Mul,
+            left: Box::new(Expression::Value(0)),
+            right: Box::new(Expression::Value(0))
+        }),
+        0
+    );
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Sub,
+            left: Box::new(Expression::Value(0)),
+            right: Box::new(Expression::Value(0))
+        }),
+        0
+    );
+}
+
+#[test]
+fn test_div() {
+    assert_eq!(
+        eval(Expression::Op {
+            op: Operation::Div,
+            left: Box::new(Expression::Value(10)),
+            right: Box::new(Expression::Value(2)),
+        }),
+        5
+    )
 }
