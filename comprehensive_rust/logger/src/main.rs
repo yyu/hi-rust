22f4e73bfd3e17f896b77a646f7b55a80ce8e79f
y² <yy@yuyuan.org>
2026-06-20 01:16:05 +0000

    logger

--- comprehensive_rust/logger/src/main.rs
+++ comprehensive_rust/logger/src/main.rs
@@ -1,4 +1,4 @@
-trait Logger {
+pub trait Logger {
     /// Log a message at the given verbosity level.
     fn log(&self, verbosity: u8, message: &str);
 }
@@ -11,22 +11,28 @@ impl Logger for StderrLogger {
     }
 }
 
-/// Only log messages up to the given verbosity level.
-struct VerbosityFilter {
-    max_verbosity: u8,
-    inner: StderrLogger,
+struct MsgFilter<F: Fn(u8, &str) -> bool> {
+    logger: StderrLogger,
+    msgfilter: F,
 }
 
-impl Logger for VerbosityFilter {
+impl<F: Fn(u8, &str) -> bool> MsgFilter<F> {
+    fn new(logger: StderrLogger, msgfilter: F) -> Self {
+        MsgFilter{logger, msgfilter}
+    }
+}
+
+impl<F: Fn(u8, &str) -> bool> Logger for MsgFilter<F> {
     fn log(&self, verbosity: u8, message: &str) {
-        if verbosity <= self.max_verbosity {
-            self.inner.log(verbosity, message);
+        if (self.msgfilter)(verbosity, message) {
+            self.logger.log(verbosity, message);
         }
     }
 }
 
 fn main() {
-    let logger = VerbosityFilter { max_verbosity: 3, inner: StderrLogger };
+    let logger = MsgFilter::new(StderrLogger, |_verbosity, msg| msg.contains("yikes"));
     logger.log(5, "FYI");
-    logger.log(2, "Uhoh");
+    logger.log(1, "yikes, something went wrong");
+    logger.log(2, "uhoh");
 }
