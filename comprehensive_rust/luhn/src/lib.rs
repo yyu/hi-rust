28ed3cbee8a070a56f747683a1a6bf26f42586e6
y² <yy@yuyuan.org>
2026-06-25 18:56:15 +0000

    luhn

--- comprehensive_rust/luhn/src/lib.rs
+++ comprehensive_rust/luhn/src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+pub fn luhn(cc_number: &str) -> bool {
+    let mut sum = 0;
+    let mut double = false;
+
+    for c in cc_number.chars().rev() {
+        if let Some(digit) = c.to_digit(10) {
+            if double {
+                let double_digit = digit * 2;
+                sum +=
+                    if double_digit > 9 { double_digit - 9 } else { double_digit };
+            } else {
+                sum += digit;
+            }
+            double = !double;
+        } else {
+            continue;
+        }
+    }
+
+    sum % 10 == 0
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_valid_cc_number() {
+        assert!(luhn("4263 9826 4026 9299"));
+        assert!(luhn("4539 3195 0343 6467"));
+        assert!(luhn("7992 7398 713"));
+    }
+
+    #[test]
+    fn test_invalid_cc_number() {
+        assert!(!luhn("4223 9826 4026 9299"));
+        assert!(!luhn("4539 3195 0343 6476"));
+        assert!(!luhn("8273 1232 7352 0569"));
     }
 }
